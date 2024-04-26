0a5908e237927a857e351271331ff8a869e3677d
Jose Rubio <jadrubio@gmail.com>
2024-04-26 17:48:15 -0400

    Adding in Ingredient List to App and passing in mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import IngredientList from "./Ingredients/IngredientList.tsx";
+import { mockRecipe } from "./mockData/mockRecipe.ts";
+
 function App() {
   return (
     <>
       <h1 className="read-the-docs">Sous App</h1>
       <p className="read-the-docs">Your helpful recipe tracking app</p>
+      <IngredientList ingredients={mockRecipe.ingredients} />
     </>
   );
 }
