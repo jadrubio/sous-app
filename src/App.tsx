1f4eed96584b6c11d77471ac62360b2627e1b54f
Jose Rubio <jadrubio@gmail.com>
2024-04-26 11:49:21 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="read-the-docs">Sous App</h1>
+      <p className="read-the-docs">Your helpful recipe tracking app</p>
+    </>
+  );
+}
+
+export default App;
