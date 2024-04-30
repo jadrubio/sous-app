ba43a8eab16383910b7e1198f1be4fc37dcd9bc6
Jose Rubio <jadrubio@gmail.com>
2024-04-30 10:35:17 -0400

    Removed bootstrap and went vanilla css

--- src/Ingredients/Ingredient.tsx
+++ src/Ingredients/Ingredient.tsx
@@ -1,34 +1,32 @@
-import { useState } from "react";
-import { IngredientProp } from "./IngredientTypes.ts";
+import {useState} from "react";
+import {IngredientProp} from "./IngredientTypes.ts";
 import "./styles.css";
 
-const Ingredient = ({ ingredient }: IngredientProp) => {
-  const { quantity, unit, item } = ingredient;
+const Ingredient = ({ingredient}: IngredientProp) => {
+  const {quantity, unit, item} = ingredient;
   const checkBoxElem = `${quantity}${unit}${item}Checkbox`;
   const [isChecked, setIsChecked] = useState(false);
   const checkedStyle = isChecked ? " checked" : "";
 
   return (
-    <>
-      <li className="list-group-item">
-        <input
-          className="form-check-input me-1"
-          type="checkbox"
-          value=""
-          onClick={() => setIsChecked(!isChecked)}
-          defaultChecked={isChecked}
-          id={checkBoxElem}
-        />
-        <label
-          className={`form-check-label ${checkedStyle}`}
-          htmlFor={checkBoxElem}
-        >
-          <span className="quantity">{quantity}</span>
-          <span className="unit">{unit}</span>
-          <span className="item">{item}</span>
-        </label>
-      </li>
-    </>
+    <li className="">
+      <input
+        className=""
+        type="checkbox"
+        value=""
+        onClick={() => setIsChecked(!isChecked)}
+        defaultChecked={isChecked}
+        id={checkBoxElem}
+      />
+      <label
+        className={checkedStyle}
+        htmlFor={checkBoxElem}
+      >
+        <span className="quantity">{quantity}</span>
+        <span className="unit">{unit}</span>
+        <span className="item">{item}</span>
+      </label>
+    </li>
   );
 };
 
--- src/Ingredients/IngredientList.tsx
+++ src/Ingredients/IngredientList.tsx
@@ -3,18 +3,34 @@ import { IngredientListType, IngredientType } from "./IngredientTypes.ts";
 import "./styles.css";
 
 const IngredientList = ({ ingredients }: IngredientListType) => {
+  const numIngredients: number = ingredients.length;
+
+  const firstColumnIngredients: IngredientType[] = ingredients.slice(0, Math.ceil(numIngredients / 2));
+  const secondColumnIngredients: IngredientType[] = ingredients.slice(Math.ceil(numIngredients / 2));
+
   return (
-    <>
+    <section>
       <h3>Ingredients</h3>
-      <ul className="list-group">
-        {ingredients.map((item: IngredientType) => (
-          <Ingredient
-            key={item.quantity + item.unit + item.item}
-            ingredient={item}
-          />
-        ))}
-      </ul>
-    </>
+      <div className="ingredient-list-group">
+        <div className="ingredient-row">
+          <article className="ingredient-column">
+            {firstColumnIngredients.map((ingredient: IngredientType, index: number) => (
+              <div key={index}>
+                <Ingredient ingredient={ingredient} />
+              </div>
+            ))}
+          </article>
+          <article className="ingredient-column">
+            {secondColumnIngredients.map((ingredient, index) => (
+              <div key={index}>
+                <Ingredient ingredient={ingredient} />
+              </div>
+            ))}
+          </article>
+        </div>
+      </div>
+    </section>
   );
 };
+
 export default IngredientList;
